f96e20f06a62cb20944f3e37e8efc81724d6557e
Guilherme Ranoya <ranoya@gmail.com>
2023-08-27 16:34:49 -0300

    separadores com linhas e não como bordas nos divs

--- plugins/docurl.js
+++ plugins/docurl.js
@@ -9,22 +9,30 @@ let docurl = function (par) {
         let xpto = `<style>
                         .tabelaparam {
                             display: grid;
-                            grid-template-columns: 120px 240px 1fr;
+                            grid-template-columns: [init] 120px 240px 1fr [fim];
                             gap: 3px 10px;
-                            border: 1px solid var(--line-separator, #efe8d6);
                         }
 
-                        .tabelaparam div {
-                            border: 1px solid var(--line-separator, #efe8d6);
+                        .linhadi {
+                            border: 0;
+                            height: 1px;
+                            width: 100%;
+                            border-bottom: 1px solid var(--line-separator, #efe8d6);
                         }
         
                     </style>`;
-        xpto += `<div style='width: calc(100vw - 50px); margin-left: 20px; margin-right: 20px; padding-bottom: 20px;>`;
+        xpto += `<div style='width: calc(100vw - 50px); margin-left: 20px; margin-right: 20px; padding-bottom: 20px;>
+        
+                 <div style='width: calc(100vw - 50px); border: 0; height: 1px; border: 1px solid var(--line-separator, #efe8d6);'></div>
+        `;
 
         for (let i = 0; i < newarr.length; i++) {
-            xpto += `<a target='_blank' href='${newarr[i].link}'>${newarr[i].documento}</a><br>`;
-            xpto += `<a target='_self' href='${newarr[i].dba}'>${newarr[i].informacao}</a><br><br>`;
-            xpto += `<div class='tabelaparam'>${newarr[i].parametros}</div><br><br>`;
+            xpto += `
+            <a target='_blank' href='${newarr[i].link}'>${newarr[i].documento}</a><br>;
+            <a target='_self' href='${newarr[i].dba}'>${newarr[i].informacao}</a><br><br>;
+            <div class='tabelaparam'>${newarr[i].parametros}</div>
+            <div class='linhadi'></div>
+            <br><br>`;
         }
 
         xpto += `</div>`;
