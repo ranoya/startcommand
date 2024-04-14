0cebbf383cd69818bc50e35a64a2640baa45d2e1
Guilherme Ranoya <ranoya@gmail.com>
2024-04-14 09:59:37 -0300

    corrigido

--- plugins/notes.js
+++ plugins/notes.js
@@ -4,9 +4,7 @@ let notes = function (par) {
   let jsonfile = `https://docs.google.com/spreadsheets/d/1ywz7XciIATPOn5cF-L51XELMxFaLmgyBZneETNCycG4/edit#gid=444640757`; // Change the URL here (imperative)
 
     getcsvdata(GoogleSheetCsvURL(jsonfile), function (jsondata) {
-      
-        console.table(jsondata);
-    
+          
           let arr = [];
           let notetags = [];
 
