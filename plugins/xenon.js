5570f0aac54bfd2392f58701f1f3cc27fe44217a
Guilherme Ranoya <ranoya@gmail.com>
2024-04-21 11:37:16 -0300

    xenon mais interessante

--- plugins/xenon.js
+++ plugins/xenon.js
@@ -4,17 +4,30 @@ let xenon = function (par) {
   let namekey = `titulo`;
 
   let jsonfile = `https://docs.google.com/spreadsheets/d/1Uy5dWDszjwuNJAGJZ55ANVCJA-ruHd8Ey_86cvHd28A/edit#gid=0`;
-  
+  let jsonfile2 = `https://docs.google.com/spreadsheets/d/1dZpKhn38u_M-m45DX5z9CKmeDcaLq4c-PpCZt7FaXpo/edit#gid=513250687`;
+
   getcsvdata(GoogleSheetCsvURL(jsonfile), function (dados) {
 
-      let newarr = select(dados, multipatterncheck_exclude, par);
       let xpto = `<div class="outputgrid">`;
-
+    
+      let newarr = select(dados, multipatterncheck_exclude, par);
+    
       for (let i = 0; i < newarr.length; i++) {
         xpto += `<a target='_blank' href='${newarr[i][linkkey]}'>${newarr[i][namekey]}`;
       }
+    
+    
+      xpto += `<span class="separaline"></span>`;
 
-      xpto += `</div>`;
-      present(xpto);
+      getcsvdata(GoogleSheetCsvURL(jsonfile2), function (jsondata) {
+        for (let i = 0; i < jsondata.length; i++) {
+          code += `<a target='_blank' href='${jsondata[i].link}' class='rsslink'><div style='margin-bottom: 16px; padding-top: 100%; background-repeat: no-repeat; background-position: center center; background-size: cover; background-image: url(${jsondata[i].imagem});'></div>${jsondata[i].titulo} • ${jsondata[i].site}<br><br></a>`;
+        }
+
+        xpto += `</div>`;
+        present(xpto);
+      });
+
+      
     });
 };
