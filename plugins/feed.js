33756759ed8539659b0c6574da80bf82b1c3d4af
Guilherme Ranoya <ranoya@gmail.com>
2024-05-12 18:06:12 -0300

    search no feed

--- plugins/feed.js
+++ plugins/feed.js
@@ -1,19 +1,17 @@
 let feed = function (par) {
-    
-  let jsonfile = `https://docs.google.com/spreadsheets/d/1aOq6gcK_QKH0TKA-DT7MJln8SwKKaK4EIv9DBB8b5ww/edit#gid=1176429967`; 
-  getcsvdata(GoogleSheetCsvURL(jsonfile), function (jsondata) {
+  let jsonfile = `https://docs.google.com/spreadsheets/d/1aOq6gcK_QKH0TKA-DT7MJln8SwKKaK4EIv9DBB8b5ww/edit#gid=1176429967`;
+  getcsvdata(GoogleSheetCsvURL(jsonfile), function (ojsondata) {
+    let jsondata = select(ojsondata, multipatterncheck_exclude, par);
 
-            let code = `
+    let code = `
 
             <div class='outputgrid'>`;
 
-            for (let i = 0; i < jsondata.length; i++) {
-                code += `<a target='_blank' href='${jsondata[i].link}' class='rsslink'><div style='margin-bottom: 16px; padding-top: 100%; background-repeat: no-repeat; background-position: center center; background-size: cover; background-image: url(${jsondata[i].imagem});'></div>${jsondata[i].titulo} • ${jsondata[i].site}<br><br></a>`;
-            }
+    for (let i = 0; i < jsondata.length; i++) {
+      code += `<a target='_blank' href='${jsondata[i].link}' class='rsslink'><div style='margin-bottom: 16px; padding-top: 100%; background-repeat: no-repeat; background-position: center center; background-size: cover; background-image: url(${jsondata[i].imagem});'></div>${jsondata[i].titulo} • ${jsondata[i].site}<br><br></a>`;
+    }
 
-            code += `</div>`;
-            present(code);
-        
-        });
-
-}
+    code += `</div>`;
+    present(code);
+  });
+};
--- plugins/pfeed.js
+++ plugins/pfeed.js
@@ -1,19 +1,17 @@
 let pfeed = function (par) {
-    
-  let jsonfile = `https://docs.google.com/spreadsheets/d/1dZpKhn38u_M-m45DX5z9CKmeDcaLq4c-PpCZt7FaXpo/edit#gid=1826705642`; 
-    getcsvdata(GoogleSheetCsvURL(jsonfile), function (jsondata) {
+  let jsonfile = `https://docs.google.com/spreadsheets/d/1dZpKhn38u_M-m45DX5z9CKmeDcaLq4c-PpCZt7FaXpo/edit#gid=1826705642`;
+  getcsvdata(GoogleSheetCsvURL(jsonfile), function (ojsondata) {
+    let jsondata = select(ojsondata, multipatterncheck_exclude, par);
 
-            let code = `
+    let code = `
 
             <div class='simplediv'>`;
 
-            for (let i = 0; i < jsondata.length; i++) {
-                code += `<a target='_blank' href='${jsondata[i].link}' class='rsslink'>${jsondata[i].titulo} • ${jsondata[i].site}</a><br>`;
-            }
+    for (let i = 0; i < jsondata.length; i++) {
+      code += `<a target='_blank' href='${jsondata[i].link}' class='rsslink'>${jsondata[i].titulo} • ${jsondata[i].site}</a><br>`;
+    }
 
-            code += `</div>`;
-            present(code);
-        
-        });
-
-}
+    code += `</div>`;
+    present(code);
+  });
+};
