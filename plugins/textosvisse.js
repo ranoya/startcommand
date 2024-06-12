7f3794129c663634f8c2ff3cb2a6b2c5952c89b6
Guilherme Ranoya <ranoya@gmail.com>
2024-06-12 11:23:13 -0300

    deu probrema aqui...

--- plugins/textosvisse.js
+++ plugins/textosvisse.js
@@ -1,51 +1,54 @@
 let textosvisse = function (par) {
   // Change the funcion name here (imperative)
 
-  let jsonfile = "https://docs.google.com/spreadsheets/d/1d9eKw82lnM5GvVkBUe8wLcHSxpmDm5B6dUqWRbazOik/edit#gid=1493544759"; // Change the URL here (imperative)
+  let jsonfile =
+    "https://docs.google.com/spreadsheets/d/1d9eKw82lnM5GvVkBUe8wLcHSxpmDm5B6dUqWRbazOik/edit#gid=1493544759"; // Change the URL here (imperative)
 
   let namekey = `titulo`; // Change the Key for the Title of the links, if needed
   let groupkey = `tipologia`; // Change the Key for the Groups of the links, if needed
   let linkkey = `link`; // Change the Key for the link url of the links, if needed
-  
 
   // Don't mess with the rest, if you don't want trouble ;-)
 
   getcsvdata(GoogleSheetCsvURL(jsonfile), function (jsondata) {
+    let dados = select(jsondata, multipatterncheck_exclude, par);
+    let selectedarr = tags(dados, groupkey, ",");
+    let code = `<div class="outputgrid" style='grid-template-columns: 1fr [fim]'><span class='categoria noline' style='border: 0;'>Biblioteca VISSE</span><span class='categoria'>&nbsp;</span>`;
 
-      let dados = select(jsondata, multipatterncheck_exclude, par);
-      let selectedarr = tags(dados, groupkey, ",");
-        let code = `<div class="outputgrid" style='grid-template-columns: 1fr [fim]'><span class='categoria noline' style='border: 0;'>Biblioteca VISSE</span><span class='categoria'>&nbsp;</span>`;
-        
-        let arr = sortbylist(dados, selectedarr, groupkey);
+    let arr = sortbylist(dados, selectedarr, groupkey);
 
-      if (arr.length > 10) {
-        for (let c = 0; c < selectedarr.length; c++) {
-            code += `
+    if (arr.length > 10) {
+      for (let c = 0; c < selectedarr.length; c++) {
+        code += `
             <span class='categoria'><a href='javascript:addinput("${selectedarr[c]}")' class='grouplink'>${selectedarr[c]}</a></span>`;
-          for (let l = 0; l < arr.length; l++) {
-            if (arr[l][groupkey] == selectedarr[c]) {
-                code += `<a target='_blank' href='${arr[l][linkkey]}' class='linksrecursos' style='grid-column: 1/fim;' >${arr[l][namekey]}</a>`;
-            }
-          }
-        }
-      } else {
-        let ultimoregistro = "";
-        code += `<span class='categoria'>`;
-        for (let c = 0; c < selectedarr.length; c++) {
-          code += `<a href='javascript:addinput("${selectedarr[c]}")' class='grouplink'>${selectedarr[c]}</a> • `;
-        }
-        code += `</span>`;
         for (let l = 0; l < arr.length; l++) {
-          if (arr[l][linkkey] != ultimoregistro) {
-              code += `<a target='_blank' href='${arr[l][linkkey]}' class='linksrecursos' style='grid-column: 1/fim;' >${arr[l][namekey]}</a>`;
-            ultimoregistro = arr[l][linkkey];
+          if (arr[l][groupkey] == selectedarr[c]) {
+            code += `<a target='_blank' href='${arr[l][linkkey]}' class='linksrecursos' style='grid-column: 1/fim;' >${arr[l][namekey]}</a>`;
           }
         }
       }
-      code += `<div>`;
-      if (arr.length == 0) {
-        code = "";
+    } else {
+      let ultimoregistro = "";
+      code += `<span class='categoria'>`;
+      for (let c = 0; c < selectedarr.length; c++) {
+        code += `<a href='javascript:addinput("${selectedarr[c]}")' class='grouplink'>${selectedarr[c]}</a> • `;
       }
-      present(code);
-    });
+      code += `</span>`;
+      for (let l = 0; l < arr.length; l++) {
+        if (arr[l][linkkey] != ultimoregistro) {
+          code += `<a target='_blank' href='${arr[l][linkkey]}' class='linksrecursos' style='grid-column: 1/fim;' >${arr[l][namekey]}</a>`;
+          ultimoregistro = arr[l][linkkey];
+        }
+      }
+    }
+    code += `<div>`;
+
+    console.table(arr);
+
+    /* if (arr.length == 0) {
+        code = "";
+      } */
+
+    present(code);
+  });
 };
